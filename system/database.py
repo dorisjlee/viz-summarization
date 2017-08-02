29b17faa74d2da64632cce01c20c01b6555c0756
marriema <414734724@qq.com>
2017-08-02 13:13:20 -0500

    vfvf

--- system/database.py
+++ system/database.py
@@ -1,7 +1,22 @@
-from flask.ext.sqlalchemy import SQLAlchemy
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/viz-summarization'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://summarization:lattice@localhost:5432'
-db = SQLAlchemy(app)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+import pandas as pd
+
+
+
+'''
+Configure Flask by providing the PostgreSQL URI so that the app is able to connect to the database, through
+'''
+
+
+
+#data = pd.read_csv('../data/titanic/titanic.csv',index_col=0)
+engine = create_engine("postgresql://summarization:lattice@localhost:5433")
+# data.to_sql(name='titanic', con=engine, if_exists = 'replace', index=False)
+
+connection = engine.connect()
+
 
 def initialize_DB():
 	'''
@@ -21,42 +36,65 @@ def upload_data():
 	'''
 	raise NotImplementedError
 def get_tables():
-	'''
-	Get a list of all the tables inside the viz-summarization folder
-	summarization=# SELECT TABLE_NAME  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND  TABLE_SCHEMA='public' ORDER BY TABLE_NAME;
-	table_name
-	------------
-	 titanic
-	(1 row)
-	We want to be able to retreive this in the front end so that the uplaod dataset dropdown menu updates dynamically.
-	'''
-	raise NotImplementedError
+    '''
+        Get a list of all the tables inside the viz-summarization folder
+        summarization=# SELECT TABLE_NAME  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND  TABLE_SCHEMA='public' ORDER BY TABLE_NAME;
+        table_name
+        ------------
+        titanic
+        (1 row)
+        We want to be able to retreive this in the front end so that the uplaod dataset dropdown menu updates dynamically.
+    '''
+    result = connection.execute("SELECT TABLE_NAME  FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' AND  TABLE_SCHEMA='public' ORDER BY TABLE_NAME;")
+    print "table"
+    print '--------'
+    ret = []
+    for row in result:
+        ret.append(str(row["table_name"]))
+    return ret
+
+ret = get_tables()
+print ret
+
+
 def get_columns(tablename):
-	'''
-	Get a list of all the columns inside a particular table to display to the front end 
-	in the x and y axis selection panel dropdown menu
+  '''
+  Get a list of all the columns inside a particular table to display to the front end 
+  in the x and y axis selection panel dropdown menu
 
-	SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '<tablename>';
+  SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '<tablename>';
 
-	e.g. 
-	summarization=# SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'titanic';
-	 column_name
-	-------------
-	 Name
-	 PClass
-	 Age
-	 Sex
-	 Survived
-	 SexCode
-	(6 rows)
-	'''
-	raise NotImplementedError
-def query_vizData(tablename,x_attr,y_attr, agg_func, filters):
-	'''
-	Constructs a typical query for each visualization 
-	1) SELECT <agg_func>(<y_attr>) FROM  <tablename> WHERE <filters> GROUPBY <x_attr> 
-	e.g. SELECT SUM(Population) FROM census WHERE RACE=Asian & GENDER=Female GROUPBY GENDER
-	2) Read retreived results and store it as a tuple
-	3) return tuples for each bar in the visualization [a1,a2,a3]
-	'''
-	raise NotImplementedError
+  e.g. 
+  summarization=# SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'titanic';
+   column_name
+  -------------
+   Name
+   PClass
+   Age
+   Sex
+   Survived
+   SexCode
+  (6 rows)
+  '''
+  result = connection.execute("SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = " + "'" + tablename + "';")
+  print "column_name"
+  print '--------'
+  ret = []
+  for row in result:
+      ret.append(str(row["column_name"]))
+  return ret
+
+
+r2 = get_columns("titanic")
+print r2
+# def query_vizData(tablename,x_attr,y_attr, agg_func, filters):
+# 	'''
+# 	Constructs a typical query for each visualization 
+# 	1) SELECT <agg_func>(<y_attr>) FROM  <tablename> WHERE <filters> GROUPBY <x_attr> 
+# 	e.g. SELECT SUM(Population) FROM census WHERE RACE=Asian & GENDER=Female GROUPBY GENDER
+# 	2) Read retreived results and store it as a tuple
+# 	3) return tuples for each bar in the visualization [a1,a2,a3]
+#   #str = "SELECT" + agg_func + "(" + y_attr + ")" + "FROM" + tablename + "WHERE" + filters + "GROUPBY" + x_attr
+#   #result = connection.execute(str)
+#   '''
+#   raise NotImplementedError
--- system/main.py
+++ system/main.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask import render_template
 from Data import getJsonFromLattice
+from database import *
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__, static_url_path = '', static_folder = 'static', template_folder = 'templates')
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://summarization:lattice@localhost:5433'
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
     ret = getJsonFromLattice()
+    table_name = get_tables()  # a list containing all the table names
+    column_name = get_columns("titanic") # a list containing all the column names
+
     print "ret: "
     print ret
-    return render_template("main.html", treeTreant2 = ret)
+    return render_template("main.html", treeTreant2 = ret, table = table_name, column = column_name)
 
 if __name__ == "__main__":
     app.run()
