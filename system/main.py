13105977d330559e85a844bf762fca0e2cbf7846
dorislee0309 <dorislee@berkeley.edu>
2017-08-16 16:55:35 -0500

    playing around with request ajax, session stuff

--- system/main.py
+++ system/main.py
@@ -13,14 +13,23 @@ db = SQLAlchemy(app)
 def getTreeJSON():
   (treeTreant, nodeDic) = getJsonFromLattice()
   session['treeTreant'] = treeTreant
-  return treeTreant
+  session['nodeDic'] = nodeDic
+  return treeTreant, nodeDic
 
+# @app.route("/getTree")
+# def getTree():
+#   (treeTreant, nodeDic) = getJsonFromLattice()
+#   session['nodeDic'] = nodeDic
+#   return nodeDic
 
-@app.route("/getColumns")
-def getColumns(tablename):
-  column_name = get_columns(tablename)
+@app.route("/getColumns", methods=['GET'])
+def getColumns():
+  print "getColumns"
+  print request.form["table_selected"]
+  column_name = get_columns(request.form["table_selected"])
+  print "column_name:",column_name
   session['column_name'] = column_name  # a list containing all the column names
-  return column_name
+  return json.dumps(column_name)
 
 '''
 @app.route("/handle_data", methods=['GET', 'POST'])
@@ -37,14 +46,31 @@ def getTables():
   session['table_name'] = table_name
   return table_name
 
-@app.route("/getTree")
-def getTree():
-  (treeTreant, nodeDic) = getJsonFromLattice()
-  session['nodeDic'] = nodeDic
-  return nodeDic
+
+@app.route("/getSelectedAxis", methods=['GET', 'POST'])
+def getSelectedAxis():
+    
+    select_avg_name = str(request.form.get('avg'))
+    session['select_avg_name'] = select_avg_name
+
+    filter_list = request.form.getlist('fields[]')
+    for key in filter_list:
+      print key
+
+    session['filter_list'] = filter_list
+    select_xaxis = str(request.form.get('xaxis_select'))
+    session['select_xaxis'] = select_xaxis
+    select_yaxis = str(request.form.get('yaxis_select'))
+    session['select_yaxis'] = select_yaxis
 
 
-
+    print "---------------"
+    print session.get('select_avg_name', None)
+    print session.get('select_yaxis', None)
+    print session.get('select_xaxis', None)
+    print session.get('select_table_name', None)
+    print session.get('filter_list', None)
+    print "----------------"
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -56,53 +82,23 @@ def index():
 
     print "ret: "
     '''
-    table_name = session.get('table_name', None)
+    
+    all_tables = getTables()
+    treeTreant, nodeDic = getTreeJSON()
+    # column_name = [""]
+    # select_table_name = str(request.form.get('table_select'))
+    # session['select_table_name'] = select_table_name
+    # if session.get('select_table_name', None) is not None:
+    #   column_name = getColumns(session.get('select_table_name', None))
+    # #if(session.get('select_table_name', None) is not None) and (len(session.get('filter_list', None)) != 0) and (session.get('select_yaxis', None) is not None) and (session.get('select_aaxis', None) is not None) and (session.get('select_avg_name', None) is not None):
+    #   #query_vizData(session.get('select_table_name', None), session.get('select_aaxis', None), session.get('select_yaxis', None), session.get('select_avg_name', None), session.get('filter_list', None))
 
-    treeTreant = session.get('treeTreant', None)
-    nodeDic = session.get('nodeDic', None)
-    column_name = [""]
+    # generateVizObj("titanic", "survived", "id", "COUNT", ["sex='male'", "age<20"])
 
-    select_table_name = str(request.form.get('table_select'))
-    session['select_table_name'] = select_table_name
-    select_avg_name = str(request.form.get('avg'))
-    session['select_avg_name'] = select_avg_name
-
-    filter_list = request.form.getlist('fields[]')
-    for key in filter_list:
-      print key
-
-    session['filter_list'] = filter_list
-    select_aaxis = str(request.form.get('xaxis_select'))
-    session['select_aaxis'] = select_aaxis
-    select_yaxis = str(request.form.get('yaxis_select'))
-    session['select_yaxis'] = select_yaxis
-
-    if session.get('select_table_name', None) is not None:
-      column_name = getColumns(session.get('select_table_name', None))
-
-    print "---------------"
-    print session.get('select_avg_name', None)
-    print session.get('select_yaxis', None)
-    print session.get('select_aaxis', None)
-    print session.get('select_table_name', None)
-    print session.get('filter_list', None)
-    print "----------------"
-
-    #if(session.get('select_table_name', None) is not None) and (len(session.get('filter_list', None)) != 0) and (session.get('select_yaxis', None) is not None) and (session.get('select_aaxis', None) is not None) and (session.get('select_avg_name', None) is not None):
-      #query_vizData(session.get('select_table_name', None), session.get('select_aaxis', None), session.get('select_yaxis', None), session.get('select_avg_name', None), session.get('filter_list', None))
-
-    generateVizObj("titanic", "survived", "id", "COUNT", ["sex='male'", "age<20"])
-
-
-    print("line 61")
-    print table_name
-    print column_name
-    return render_template("main.html", treeTreant2 = treeTreant, table = json.loads(table_name), column = json.loads(column_name),
-nodeDic = nodeDic)
-
-
-
-
+    # return render_template("main.html", treeTreant2 = treeTreant, all_tables = all_tables,\
+    #                         column = json.loads(column_name), nodeDic = nodeDic)
+    return render_template("main.html", treeTreant2 = treeTreant, all_tables = all_tables,\
+                            nodeDic = nodeDic)
 
 if __name__ == "__main__":
 
