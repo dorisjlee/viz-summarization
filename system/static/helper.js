7062bcd29bbbcb28aff5d65a5ba387e3f1ad0e11
dorislee0309 <dorislee@berkeley.edu>
2017-08-18 10:23:57 -0500

    get post request working now

---
+++ system/static/helper.js
@@ -0,0 +1,21 @@
+var columns =[];
+$("#all_tables").change(
+    $.post('/getColumns',{
+        table_name: 'titanic'
+    }).success( function(data){
+        // console.log(data)
+        columns = JSON.parse(data);
+        console.log(columns)
+        populateOptions(columns,document.getElementById("xaxis"));
+        populateOptions(columns,document.getElementById("yaxis"));
+    })
+)
+
+function populateOptions(list,selector)
+{
+    console.log(selector.length)
+    for (var i=0;i<list.length;i++){
+      selector.options[i+1] = new Option(list[i],list[i]);
+    }
+    console.log("added options")
+}
