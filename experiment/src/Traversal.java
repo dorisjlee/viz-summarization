8e1a6ed2312dda89e379fa4494265f8cd93b48ac
dorisjlee <dorisjunglinlee@gmail.com>
2017-10-04 06:33:28 -0500

    iterative solution doesn't seem to traverse downwards

--- experiment/src/Traversal.java
+++ experiment/src/Traversal.java
@@ -3,14 +3,8 @@ import java.util.Collections;
 import java.util.HashMap;
 
 public class Traversal {
-	// Traversal methods take in a hashmap representing the materialized graph and return a subgraph 
-//	public static void main(String[] args) {
-//		System.out.print("Hello world");
-//		HashMap<String, ArrayList<Double>>  hmap = Hierarchia.computeVisualizationMap();
-//		Hierarchia.print_map(hmap);
-//		Euclidean ed = new Euclidean();
-//		greedyPicking(hmap,ed);
-//	}
+	// Traversal methods take in a hashmap representing the materialized graph and return a maximal subgraph (list of node indices)
+
 	public static double[] ArrayList2Array(ArrayList<Double> arrList) {
 		 double[] target = new double[arrList.size()];
 		 for (int i = 0; i < target.length; i++) {
@@ -20,7 +14,7 @@ public class Traversal {
 	}
 	public static void greedyPicking(HashMap<String, ArrayList<Double>> id2MetricMap,  
 									ArrayList<Node> nodeList, HashMap<String, Integer> id2IDMap ,Distance metric) {
-		ArrayList<Integer> maxSubgraph =new ArrayList(); 
+		ArrayList<Integer> maxSubgraph =new ArrayList<Integer>(); 
 		System.out.println("---------------- Greedy Picking -----------------");
 		// Variable Initialization
 		double [] parentVal;
@@ -29,31 +23,21 @@ public class Traversal {
 		Node parentNode;
 		Node childNode;
 		double utility;
+		double maxSubgraphUtility=0;
 		// Start from root (nodeID=0)
-		double [] parentVal =  ArrayList2Array(id2MetricMap.get("#"));
-		Integer parentID = id2IDMap.get("#");
-		Node parentNode = nodeList.get(parentID);
+		parentVal =  ArrayList2Array(id2MetricMap.get("#"));
+		parentID = id2IDMap.get("#");
+		parentNode = nodeList.get(parentID);
 		maxSubgraph.add(parentID); // maxSubgraph must contain root
-		while (parentNode.get_child_list()!=null) { // Terminate when hit leaf nodes (with no children)
-			if (parentID!=0) {
-				// Start from root (nodeID=0)
-				parentVal =  ArrayList2Array(id2MetricMap.get("#"));
-				parentID = id2IDMap.get("#");
-				parentNode = nodeList.get(parentID);
-				maxSubgraph.add(parentID); // maxSubgraph must contain root
-			}
+		while (parentNode.get_child_list().size()!=0) { // Terminate when hit leaf nodes (with no children)
 			ArrayList<Integer> children = parentNode.get_child_list();
 			HashMap<Integer,Double> childUtilities =new HashMap<Integer,Double> (); 
 			for (Integer childID: children) {
-//				System.out.println(childID);
 				childNode =  nodeList.get(childID);
 				childVal = ArrayList2Array(id2MetricMap.get(childNode.get_id()));
 				utility = metric.computeDistance(parentVal, childVal);
-//				System.out.println(utility);
 				childUtilities.put(childID,utility);
 			}
-			// System.out.print(Collections.max(childUtilities.values()));
-			
 			// Find the child with the max utility
 			HashMap.Entry<Integer, Double> maxEntry = null;
 			for (HashMap.Entry<Integer, Double> entry : childUtilities.entrySet())
@@ -65,17 +49,34 @@ public class Traversal {
 			}
 			System.out.println(maxEntry.getKey()); // max child ID
 			System.out.println(maxEntry.getValue()); // max child's utility
+			maxSubgraphUtility+=maxEntry.getValue();
+			// Best Child is now the new parent
+			parentID = maxEntry.getKey();
+			parentNode = nodeList.get(parentID);
+			parentVal =  ArrayList2Array(id2MetricMap.get(parentNode.get_id()));
+			maxSubgraph.add(parentID);  // adding this best child into the subgraph
+		}
+		// Summary of maximum subgraph 
+		System.out.print("Max Subgraph: [");
+		for (int j =0 ; j< maxSubgraph.size();j++) {
+			if (j==maxSubgraph.size()-1) {
+				System.out.print(Integer.toString(maxSubgraph.get(j))+"]\n");
+			}else {
+				System.out.print(Integer.toString(maxSubgraph.get(j))+",");
+			}
+		}
+		System.out.print("[");
+		for (int j =0 ; j< maxSubgraph.size();j++) {
+			if (j==maxSubgraph.size()-1) {
+				System.out.print(nodeList.get(j).get_id()+"]\n");
+			}else {
+				System.out.print(nodeList.get(j).get_id()+",");
+			}
 		}
 		
-	}
-	// Find children, compute utility, pick best children, repeat
-	
-	//	double [] parentVals = id2MetricMap.get("A$1#C$0#D$1#E$0#G$1#");
-	//	double [] childVals = id2MetricMap.get("A$1#C$0#D$1#E$0#G$1#");
-	//	double utility = metric.computeDistance(parentVals, childVals);
-	//	System.out.println("done!");
-	// base case if no children, then end
-	
-	
-	
+		
+		
+		System.out.println("Total Utility:"+Double.toString(maxSubgraphUtility));
+		
+	}	
 }
