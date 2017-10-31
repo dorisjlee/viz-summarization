1d6f3362644fa81d579c02145b5c38f39e38ad6a
dorisjlee <dorisjunglinlee@gmail.com>
2017-10-31 09:15:08 -0500

    running experiments

--- experiment/src/Experiment.java
+++ experiment/src/Experiment.java
@@ -14,6 +14,7 @@ public class Experiment {
 	Hierarchia h;
 	Traversal algo;
 	String fname;
+	int nbars;
 	public Experiment(String datasetName, String xAxisName, String yAxisName, int k, String algoName, Distance dist,
 			double iceberg_ratio, double informative_critera) throws SQLException {
 		super();
@@ -28,10 +29,15 @@ public class Experiment {
 		this.distName = dist.getDistName();
 		this.h = new Hierarchia(datasetName,xAxisName);
 		this.lattice = Hierarchia.generateFullyMaterializedLattice(dist,iceberg_ratio,informative_critera);
+		this.nbars = lattice.id2MetricMap.get("#").size();
 		if (this.algoName.equals("frontierGreedy")) {
-		   this.algo = new FrontierGreedyPicking(lattice,dist);   
+			this.algo = new FrontierGreedyPicking(lattice,dist);   
+		}else if (this.algoName.equals("greedy")) {
+			this.algo = new GreedyPicking(lattice,dist);
+		}else if (this.algoName.equals("naiveGreedy")) {
+			this.algo = new NaiveGreedyPicking(lattice, dist);
 		}
-	    this.fname = datasetName+"_"+xAxisName+"_"+algoName+"_"+distName+"_ic"+iceberg_ratio+"_ip"+informative_critera+"_k"+k+".json"; 
+	    this.fname = datasetName+"_"+xAxisName.replace("_","-")+"_"+algoName+"_"+distName+"_ic"+iceberg_ratio+"_ip"+informative_critera+"_k"+k+"_nbar"+nbars+".json"; 
 	}
 	public void runOutput() {
 		algo.pickVisualizations(k);
@@ -41,7 +47,21 @@ public class Experiment {
 	}
 	public static void main(String[] args) throws SQLException 
 	{
-	   Experiment exp = new Experiment("titanic", "survived", "COUNT (id)", 10, "frontierGreedy", new Euclidean(),0.1,0.8);
-       exp.runOutput();
+	   // Testing different algo on different datasets
+	   int k = 20;
+       String algo = "frontierGreedy"; 
+	   //String algo = "naiveGreedy";
+	   //String algo = "greedy";
+	   Experiment exp = new Experiment("titanic", "survived", "COUNT(id)", k, algo, new Euclidean(),0.1,0.8);
+	   exp.runOutput();
+	   exp = new Experiment("turn", "has_list_fn", "SUM(slots_millis_reduces)", k, algo, new Euclidean(),0.1,0.8);
+	   exp.runOutput();
+	   exp = new Experiment("mushroom","type", "COUNT", k, algo, new Euclidean(),0.1,0.8);
+	   exp.runOutput();
+	   exp = new Experiment("mushroom","cap_surface", "COUNT", k, algo, new Euclidean(),0.1,0.8);
+	   exp.runOutput();   
+	   //}
+	   
+	   
 	}
 }
