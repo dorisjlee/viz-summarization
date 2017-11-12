e488a48cf19210b86e619a6d2dfd87e9f81dd3b6
Huizi Hu <huizihu0721@gmail.com>
2017-11-12 16:47:48 -0600

    Add basic sliders

--- system/static/helper.js
+++ system/static/helper.js
@@ -84,6 +84,15 @@ $("#all_tables").change(function (){
         populateOptions(data["k"],document.getElementById("k"));
     })
 })
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
+
 // $("#submit").click(constructQuery)
 // $("#xaxis").change(constructQuery)
 // $("input[name='aggFunc']").change(constructQuery)
