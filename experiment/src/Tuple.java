dcea687435abe3f3dbf077b3d90fd5cb212d7a7e
dorisjlee <dorisjunglinlee@gmail.com>
2017-11-14 14:12:36 -0600

    probabilistic picking unfinished

---
+++ experiment/src/Tuple.java
@@ -0,0 +1,20 @@
+public class Tuple {
+	public Integer x;
+	public Float y;
+	public Tuple(Integer x, Float y) {
+		this.x = x;
+		this.y = y;
+	}
+	public Integer getX() {
+		return x;
+	}
+	public void setX(Integer x) {
+		this.x = x;
+	}
+	public Float getY() {
+		return y;
+	}
+	public void setY(Float y) {
+		this.y = y;
+	}
+}
