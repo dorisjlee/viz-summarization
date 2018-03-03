b07c36a0c76a6208aeafe4e6767407412b4e8133
dorisjlee <dorisjunglinlee@gmail.com>
2018-03-02 19:03:14 -0600

    finished police stop dataset

--- experiment/src/evaluation/UserStudyBaseline.java
+++ experiment/src/evaluation/UserStudyBaseline.java
@@ -1,7 +1,9 @@
 package evaluation;
+
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,17 +27,31 @@ public class UserStudyBaseline {
 	   Experiment exp;
 	   int k =10;
 	   String aggFunc="SUM";
-	   // Dataset #1  // Dataset #2 
 	   Experiment.experiment_name="../ipynb/dashboards/json/UserStudyBaseline";
-	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList( "is_multi_query","is_profile_query","is_event_query","has_impressions_tbl",
-			   	"has_clicks_tbl","has_actions_tbl","has_distinct","has_list_fn"));
-	   String yAxis = "slots_millis_reduces";
-	   String xAxis = "has_list_fn";
+	   // Dataset #1  
+//	   String dataset_name = "turn";
+//	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList( "is_multi_query","is_profile_query","is_event_query","has_impressions_tbl",
+//			   	"has_clicks_tbl","has_actions_tbl","has_distinct","has_list_fn"));
+//	   String yAxis = "slots_millis_reduces";
+//	   String xAxis = "has_list_fn";
 	   
+	   // Dataset #2 
+	   // Generate ct_police_stop.csv 
+	   Database db = new Database();
+	   //ArrayList<String> colArrs = new ArrayList<String>(Arrays.asList("driver_gender", "driver_race","contraband_found", "stop_outcome", "is_arrested", "stop_duration"));
+	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList("driver_gender", "driver_race", "search_conducted", "contraband_found", "stop_outcome", "is_arrested", "stop_duration", "stop_time_of_day", "speeding_violations", "other_violations", "registration_plates_violations", "moving_violation", "cell_phone_violations", "lights_violations", "seat_belt_violations", "stop_sign_light_violations", "safe_movement_violations", "equipment_violations", "driver_age_category"));
+	   ResultSet rs = Database.viz_query("ct_police_stop", groupby, "stop_outcome", "COUNT", new ArrayList<String>(Arrays.asList()));
+	   
+	   Database.resultSet2csv(rs,"ct_police_stop",groupby,"COUNT");
+	   System.out.println("Finished Materializing GroupBy Results");
+	   String dataset_name = "ct_police_stop";
+	   String yAxis = "id";
+	   String xAxis = "stop_outcome";
+	   String aggType = "COUNT";
 	   
 	   Distance dist = new Euclidean();
 	   Traversal BFS = new RandomWalk();
-	   exp = new Experiment("turn", xAxis, yAxis,groupby,"SUM", k, BFS, dist,0,0.1,false);
+	   exp = new Experiment(dataset_name, xAxis, yAxis,groupby,aggType, k, BFS, dist,0,0.1,false);
 	   exp.runTableLayoutOutput(exp);   
 	   
 //	   Traversal clustering = new BaselineKmeans();
