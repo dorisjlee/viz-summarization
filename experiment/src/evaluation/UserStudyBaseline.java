adfbe504bdde22c8eb9dbf4d045aa2de63f23166
dorisjlee <dorisjunglinlee@gmail.com>
2018-03-05 16:32:05 -0600

    tested different datasets

--- experiment/src/evaluation/UserStudyBaseline.java
+++ experiment/src/evaluation/UserStudyBaseline.java
@@ -28,35 +28,47 @@ public class UserStudyBaseline {
 	   Experiment exp;
 	   int k =10;
 	   Experiment.experiment_name="../ipynb/dashboards/json/UserStudyBaseline";
-	   // Dataset #1  
-//	   String dataset_name = "turn";
-//	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList( "is_multi_query","is_profile_query","is_event_query","has_impressions_tbl",
-//			   	"has_clicks_tbl","has_actions_tbl","has_distinct","has_list_fn"));
-//	   String yAxis = "slots_millis_reduces";
-//	   String xAxis = "has_list_fn";
-	   
-	   // Dataset #2 
-	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList(
-		"state", "driver_gender", "driver_race", "search_conducted",
-		"contraband_found", "is_arrested", "stop_duration", 
-		"stop_time_of_day", "driver_age_category"));
-	   // "speeding_violations", "other_violations"
-	   //"registration_plates_violations", "moving_violation","cell_phone_violations"
-	   //"stop_outcome", 
-	   
-	   // Generate ct_police_stop.csv 
-	   Database db = new Database();
-	   ResultSet rs = Database.viz_query("ct_police_stop", groupby, "stop_outcome", "COUNT", new ArrayList<String>(Arrays.asList()));
-	   Database.resultSet2csv(rs,"ct_police_stop",groupby,"COUNT");
-	   System.out.println("Finished Materializing GroupBy Results");
-	   String dataset_name = "ct_police_stop";
-	   String yAxis = "id";
-	   String xAxis = "is_arrested";//"stop_outcome", 
-	   String aggType = "COUNT";
+	   // Dataset #1 : Turn
+	   String dataset_name = "turn";
+	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList( "is_multi_query","is_profile_query","is_event_query","has_impressions_tbl",
+			   	"has_clicks_tbl","has_actions_tbl","has_distinct","has_list_fn"));
+	   String yAxis = "slots_millis_reduces";
+	   String xAxis = "has_list_fn";
+	   String aggType = "SUM";
 	   Distance dist = new Euclidean();
+	   // Dataset #2 : Police Stop
+//	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList(
+//		"driver_gender", "driver_race", "search_conducted",
+//		"contraband_found", "is_arrested", "stop_duration", 
+//		"stop_time_of_day", "driver_age_category"));
+//	   // "speeding_violations", "other_violations"
+//	   //"registration_plates_violations", "moving_violation","cell_phone_violations"
+//	   //"stop_outcome", 
+//	   String dataset_name = "ct_police_stop";
+//	   String yAxis = "id";
+//	   String xAxis = "is_arrested";//"stop_outcome", 
+//	   String aggType = "COUNT";
+//	   Distance dist = new Euclidean();
+	   
+	   // Dataset #3 : Mushroom 
+//	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList("type","cap_shape", "cap_surface" , "cap_color" , "bruises" , "odor"));
+//	   String dataset_name = "mushroom";
+//	   String yAxis = "type";
+//	   String xAxis = "cap_surface"; 
+//	   String aggType = "COUNT";
+//	   Distance dist = new Euclidean();
+	   
+	   // Dataset #3 : Titanic 
+//	   ArrayList<String> groupby = new ArrayList<String>(Arrays.asList("survived","sexcode","pc_class"));
+//	   String dataset_name = "titanic";
+//	   String yAxis = "id";
+//	   String xAxis = "survived"; 
+//	   String aggType = "COUNT";
+//	   Distance dist = new Euclidean();
+	   
 	   	   
 	   Traversal ourAlgo = new BreadthFirstPicking();
-	   exp = new Experiment(dataset_name, xAxis, yAxis,groupby,aggType, k, dist,0,0.8,false);
+	   exp = new Experiment(dataset_name, xAxis, yAxis,groupby,aggType, k, dist,0,0.9,false);
 	   exp.setAlgo(ourAlgo);
 	   exp.runOutput(exp);
 	   
