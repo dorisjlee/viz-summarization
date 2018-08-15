94835813a37bba532c8dbd18463c93671d35d5ed
dorisjlee <dorisjunglinlee@gmail.com>
2018-08-14 21:02:11 -0500

    trying to use vegalite to plot mushroom data

--- survey/src/client/question.js
+++ survey/src/client/question.js
@@ -32,8 +32,75 @@ survey.onComplete
     });
 
 $("#q1").Survey({model: survey});
-// If you are asked to predict the distribution of mushroom type for “Cap color= c; Gill Spacing = c”, which one of these five visualization would you chose?
-// Overall Distribution
-// Cap color = b & Gill Spacing = c
-// Cap Color = c &  Gill Spacing = c &  Cap Surface = y
-// Gill Spacing = c / Cap Color = c
+
+var json = {
+    questions: [
+        {
+            type: "radiogroup",
+            name: "expectation",
+            title: "If you are asked to predict the distribution of mushroom type for “Cap color= c; Gill Spacing = c”, which one of these five visualization would you chose?",
+            isRequired: true,
+            colCount: 1,
+            choices: [
+                "Overall Distribution",
+                "Cap color = b & Gill Spacing = c",
+                "Cap Color = c &  Gill Spacing = c &  Cap Surface = y",
+                "Gill Spacing = c / Cap Color = c"
+            ]
+        }
+    ]
+};
+
+// Attempting normalization by percentage
+// var vspec =
+//     {
+// 		"$schema" : "https://vega.github.io/schema/vega-lite/v2.json",
+//         "data" : {"values" :data },
+//         "transform": [{
+//             "window": [{
+//                 "op": "sum",
+//                 "field": "NumRecord",
+//                 "as": "TotalRecords"
+//             }],
+//             "frame": [null, null]
+//           },
+//           {
+//             "calculate": "datum.NumRecord/datum.TotalRecords * 100",
+//             "as": "PercentOfTotal"
+//           }],
+//         "layer": [
+//         	{"mark":"bar",
+//         	 "encoding": {
+//                 "x":{
+//                     "field": "type",
+//                     "type": "nominal"
+//                 },
+//                 "y":{
+//                     "field": "PercentOfTotal",
+//                     "type": "quantitative"
+//                 }
+//               }
+//     		}
+// 		]
+// 	}
+var vspec =
+    {
+		"$schema" : "https://vega.github.io/schema/vega-lite/v2.json",
+        "data" : {"values" :data },
+        "layer": [
+        	{"mark":"bar",
+        	 "encoding": {
+                "x":{
+                     "aggregate": "count",
+                     "field": "Number of Records",
+                     "type": "quantitative"
+                },
+                "y":{
+                    "field": "type",
+                    "type": "nominal"
+                }
+              }
+    		}
+		]
+	}
+vegaEmbed("#testContainer",vspec,{"actions" : false})
